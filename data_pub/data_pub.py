94bf8d3e31082dc33297ec06a1bc4c8cd9270d3e
liuhuxian <liuhuxian@gmail.com>
2024-08-20 18:13:17 +0800

    change thread logic, change printer

--- data_pub/data_pub.py
+++ data_pub/data_pub.py
@@ -1,8 +1,7 @@
-
 import rclpy
 from rclpy.node import Node
-from rclpy.executors import SingleThreadedExecutor 
-from std_msgs.msg import Float64MultiArray 
+from rclpy.executors import SingleThreadedExecutor
+from std_msgs.msg import Float64MultiArray
 from data_pub.utils import MasterRobot
 import time
 
@@ -10,65 +9,101 @@ import time
 class ArmPublisher(Node):
 
     def __init__(self):
-        super().__init__('arm_publisher')
-        self.arm1_publisher = self.create_publisher(Float64MultiArray , 'master_left/joint_states', 10)
-        self.arm2_publisher = self.create_publisher(Float64MultiArray , 'master_right/joint_states', 10)
-        self.arm1 = MasterRobot("master_left")
-        self.get_logger().info(f'Initializing the left master bot, it will take about a minute..........')
+        super().__init__("arm_publisher")
+        self.arm1_publisher = self.create_publisher(
+            Float64MultiArray, "master_left/joint_states", 10
+        )
+        self.arm2_publisher = self.create_publisher(
+            Float64MultiArray, "master_right/joint_states", 10
+        )
+        publish_frequency = 200
+        self.timer_period = 1.0 / publish_frequency
+
+        self.arm1 = MasterRobot(
+            "master_left", self.get_logger().info, self.timer_period
+        )
+        self.get_logger().info(
+            f"Initializing the left master bot, it will take about a minute.........."
+        )
         self.arm1.initialize_servos()
-        self.get_logger().info(f'Initialization complete!')
-        self.arm1.set_damping_mode()  
-        self.arm2 = MasterRobot('master_right')
-        self.get_logger().info(f'Initializing the right master bot, it will take about a minute..........!')
+        self.get_logger().info(f"Initialization complete!")
+        self.arm1.set_damping_mode()
+        self.arm1.start_read_robot_data()
+
+        self.arm2 = MasterRobot(
+            "master_right", self.get_logger().info, self.timer_period
+        )
+        self.get_logger().info(
+            f"Initializing the right master bot, it will take about a minute..........!"
+        )
         self.arm2.initialize_servos()
-        self.get_logger().info(f'Initialization complete!')
-        self.arm2.set_damping_mode() 
-        publish_frequency=100
-        self.timer_period = 1.0 / publish_frequency  
+        self.get_logger().info(f"Initialization complete!")
+        self.arm2.set_damping_mode()
+        self.arm2.start_read_robot_data()
+
         self.timer1 = self.create_timer(self.timer_period, self.publish_arm1_info)
         self.timer2 = self.create_timer(self.timer_period, self.publish_arm2_info)
+
+    def __del__(self):
+        self.arm1.stop_read_robot_data()
+        self.arm2.stop_read_robot_data()
 
     def publish_arm1_info(self):
         # now = self.get_clock().now()
         # timestamp_ = now.seconds_nanoseconds()[0] + now.seconds_nanoseconds()[1] * 1e-9
         timestamp_ = time.time()
-        arm1_info = self.arm1.get_robot_data() 
+        arm1_info = self.arm1.get_robot_data()
         # self.get_logger().info(f'Publishing arm1: {timestamp_}')
-        self.get_logger().info(f'arm1_info: {arm1_info[0]+[arm1_info[1]]} type: {type(arm1_info)}')
-        if not (isinstance(arm1_info, tuple) and len(arm1_info) == 2 and isinstance(arm1_info[0], list) and isinstance(arm1_info[1], float)):
-            self.get_logger().error('arm1_info is not in the expected format')
+        # self.get_logger().info(f'arm1_info: {arm1_info[0]+[arm1_info[1]]} type: {type(arm1_info)}')
+        if not (
+            isinstance(arm1_info, tuple)
+            and len(arm1_info) == 2
+            and isinstance(arm1_info[0], list)
+            and isinstance(arm1_info[1], float)
+        ):
+            self.get_logger().error("arm1_info is not in the expected format")
             return
         arm1_data = arm1_info[0] + [arm1_info[1]]
         arm1_data.append(timestamp_)
         if not all(isinstance(i, float) for i in arm1_data):
-            self.get_logger().error('Not all elements in the first part of arm1_info are floats')
+            self.get_logger().error(
+                "Not all elements in the first part of arm1_info are floats"
+            )
             return
-       
+
         msg = Float64MultiArray()
         msg.data = arm1_data
         self.arm1_publisher.publish(msg)
         # self.get_logger().info(f'Publishing arm1: {msg.data}')
-    
+
     def publish_arm2_info(self):
         # now = self.get_clock().now()
         # _timestamp = now.seconds_nanoseconds()[0] + now.seconds_nanoseconds()[1] * 1e-9
         _timestamp = time.time()
-        arm2_info = self.arm2.get_robot_data() 
-        if not (isinstance(arm2_info, tuple) and len(arm2_info) == 2 and isinstance(arm2_info[0], list) and isinstance(arm2_info[1], float)):
-            self.get_logger().error('arm2_info is not in the expected format')
+        arm2_info = self.arm2.get_robot_data()
+        if not (
+            isinstance(arm2_info, tuple)
+            and len(arm2_info) == 2
+            and isinstance(arm2_info[0], list)
+            and isinstance(arm2_info[1], float)
+        ):
+            self.get_logger().error("arm2_info is not in the expected format")
             return
         if not all(isinstance(i, float) for i in arm2_info[0]):
-            self.get_logger().error('Not all elements in the first part of arm2_info are floats')
+            self.get_logger().error(
+                "Not all elements in the first part of arm2_info are floats"
+            )
             return
         arm2_data = arm2_info[0] + [arm2_info[1]]
         arm2_data.append(_timestamp)
         if not all(isinstance(i, float) for i in arm2_data):
-                self.get_logger().error('Not all elements in arm2_info are floats')
-                return 
+            self.get_logger().error("Not all elements in arm2_info are floats")
+            return
         msg = Float64MultiArray()
         msg.data = arm2_data
         self.arm2_publisher.publish(msg)
         # self.get_logger().info(f'Publishing arm2: {msg.data}')
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -81,6 +116,6 @@ def main(args=None):
         node.destroy_node()
         rclpy.shutdown()
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
--- data_pub/utils.py
+++ data_pub/utils.py
@@ -1,14 +1,23 @@
 import sys
-sys.path.append('./uservo.py')
+
+sys.path.append("./uservo.py")
 import Robot
 import serial
 import threading
 from data_pub.uservo import UartServoManager
+import time
 
-class MasterRobot():
 
-    def __init__(self, robot_name):
+class MasterRobot:
+
+    def __init__(self, robot_name, printer, timer_period):
+        self.joint_pos = None
+        self.grip_percentage = None
+        self.threads = None
         self.robot_name = robot_name
+        self.printer = printer
+        self.timer_period = timer_period
+        self.read_data_state = 0
         if self.robot_name == "master_left":
             self.SERVO_PORTS_ARM = [
                 "/dev/ttyCH9344USB0",
@@ -18,8 +27,8 @@ class MasterRobot():
                 "/dev/ttyCH9344USB4",
                 "/dev/ttyCH9344USB5",
             ]
-            self.SERVO_PORTS_GRIPPER = ["/dev/ttyCH9344USB6", "/dev/ttyCH9344USB7"] 
-            # self.SERVO_PORTS_GRIPPER = ["/dev/ttyCH9344USB6"] 
+            self.SERVO_PORTS_GRIPPER = ["/dev/ttyCH9344USB6", "/dev/ttyCH9344USB7"]
+            # self.SERVO_PORTS_GRIPPER = ["/dev/ttyCH9344USB6"]
             self.SERVO_IDS_ARM = [0, 1, 2, 3, 4, 5]
             self.SERVO_IDS_GRIPPER = [6, 17]
             # self.SERVO_IDS_GRIPPER = [6]
@@ -38,43 +47,51 @@ class MasterRobot():
                 "/dev/ttyCH9344USB12",
                 "/dev/ttyCH9344USB13",
             ]
-            self.SERVO_PORTS_GRIPPER = ["/dev/ttyCH9344USB14", "/dev/ttyCH9344USB15"] 
-            # self.SERVO_PORTS_GRIPPER = ["/dev/ttyCH9344USB14"] 
+            self.SERVO_PORTS_GRIPPER = ["/dev/ttyCH9344USB14", "/dev/ttyCH9344USB15"]
+            # self.SERVO_PORTS_GRIPPER = ["/dev/ttyCH9344USB14"]
             self.SERVO_IDS_ARM = [10, 11, 12, 13, 14, 15]
             self.SERVO_IDS_GRIPPER = [16, 7]
             # self.SERVO_IDS_GRIPPER = [16]
-            self.MASTER_HOME_POS_ARM = [-46, 93, -82, -4, -96, 14]   
+            self.MASTER_HOME_POS_ARM = [-46, 93, -82, -4, -96, 14]
             self.MASTER_OPEN = 5
             self.MASTER_CLOSE = 59
             self.SLAVE_OPEN = -32
             self.SLAVE_CLOSE = 41
-          
-     
+
         self.MASTER_HOME_POS_GRIPPER = [self.MASTER_OPEN, self.SLAVE_OPEN]
-        self.SLAVE_HOME_POS_ARM = [45.0, -90.0, -90.0, -0.0, 90.0, 0.0]  # 从机械臂初始角度 
-        self.SERVO_BAUDRATE = 115200 
+        self.SLAVE_HOME_POS_ARM = [
+            45.0,
+            -90.0,
+            -90.0,
+            -0.0,
+            90.0,
+            0.0,
+        ]  # 从机械臂初始角度
+        self.SERVO_BAUDRATE = 115200
         self.uart_managers_arm = []
         self.uart_managers_gripper = []
-    
+
     def initialize_servos(self):
-     
+
         for port in self.SERVO_PORTS_ARM:
             try:
-                print(f"初始化左机械臂串口 {port}")
-                uart = serial.Serial(port=port, 
-                                     baudrate=self.SERVO_BAUDRATE,
-                                     parity=serial.PARITY_NONE, 
-                                     stopbits=1, 
-                                     bytesize=8, 
-                                     timeout=0)
+                self.printer(f"初始化左机械臂串口 {port}")
+                uart = serial.Serial(
+                    port=port,
+                    baudrate=self.SERVO_BAUDRATE,
+                    parity=serial.PARITY_NONE,
+                    stopbits=1,
+                    bytesize=8,
+                    timeout=0,
+                )
                 self.uart_managers_arm.append(UartServoManager(uart))
-                print(f"左机械臂串口 {port} 初始化成功")
+                self.printer(f"左机械臂串口 {port} 初始化成功")
             except serial.SerialException as e:
-                print(f"左机械臂串口 {port} 初始化失败: {e}")
+                self.printer(f"左机械臂串口 {port} 初始化失败: {e}")
 
         for port in self.SERVO_PORTS_GRIPPER:
             try:
-                print(f"初始化串口 {port}")
+                self.printer(f"初始化串口 {port}")
                 uart = serial.Serial(
                     port=port,
                     baudrate=self.SERVO_BAUDRATE,
@@ -84,13 +101,13 @@ class MasterRobot():
                     timeout=0,
                 )
                 self.uart_managers_gripper.append(UartServoManager(uart))
-                print(f"串口 {port} 初始化成功")
+                self.printer(f"串口 {port} 初始化成功")
             except serial.SerialException as e:
-                print(f"串口 {port} 初始化失败: {e}")
+                self.printer(f"串口 {port} 初始化失败: {e}")
 
     def set_initial_positions(self):
         # 设置机械臂舵机初始角度ba
-        print("设置机械臂舵机初始角度")
+        self.printer("设置机械臂舵机初始角度")
         for i, uservo in enumerate(self.uart_managers_arm):
             uservo.set_servo_angle(
                 self.SERVO_IDS_ARM[i],
@@ -100,28 +117,34 @@ class MasterRobot():
                 t_dec=500,
             )
             uservo.wait()  # 等待舵机静止
-            print(f"舵机 {self.SERVO_IDS_ARM[i]} 设为初始角度 {self.MASTER_HOME_POS_ARM[i]}")
+            self.printer(
+                f"舵机 {self.SERVO_IDS_ARM[i]} 设为初始角度 {self.MASTER_HOME_POS_ARM[i]}"
+            )
 
         # 设置夹爪舵机初始角度
-        print("设置夹爪舵机初始角度")
+        self.printer("设置夹爪舵机初始角度")
         for i, uservo in enumerate(self.uart_managers_gripper):
             uservo.set_servo_angle(
                 self.SERVO_IDS_GRIPPER[i], self.MASTER_HOME_POS_GRIPPER[i], interval=0
             )  # 设置舵机角度 极速模式
             uservo.wait()  # 等待舵机静止
-            print(f"舵机 {self.SERVO_IDS_GRIPPER[i]} 设为初始角度 {self.MASTER_HOME_POS_GRIPPER[i]}")
-        print("设置夹爪舵机初始角度")
+            self.printer(
+                f"舵机 {self.SERVO_IDS_GRIPPER[i]} 设为初始角度 {self.MASTER_HOME_POS_GRIPPER[i]}"
+            )
+        self.printer("设置夹爪舵机初始角度")
         for i, uservo in enumerate(self.uart_managers_gripper):
             uservo.set_servo_angle(
                 self.SERVO_IDS_GRIPPER[i], self.MASTER_HOME_POS_GRIPPER[i], interval=0
             )  # 设置舵机角度 极速模式
             uservo.wait()  # 等待舵机静止
-            print(f"舵机 {self.SERVO_IDS_GRIPPER[i]} 设为初始角度 {self.MASTER_HOME_POS_GRIPPER[i]}")
+            self.printer(
+                f"舵机 {self.SERVO_IDS_GRIPPER[i]} 设为初始角度 {self.MASTER_HOME_POS_GRIPPER[i]}"
+            )
 
         # 初始化夹爪舵机
         for port in self.SERVO_PORTS_GRIPPER:
             try:
-                print(f"初始化串口 {port}")
+                self.printer(f"初始化串口 {port}")
                 uart = serial.Serial(
                     port=port,
                     baudrate=self.SERVO_BAUDRATE,
@@ -131,83 +154,94 @@ class MasterRobot():
                     timeout=0,
                 )
                 self.uart_managers_gripper.append(UartServoManager(uart))
-                print(f"串口 {port} 初始化成功")
+                self.printer(f"串口 {port} 初始化成功")
             except serial.SerialException as e:
-                print(f"串口 {port} 初始化失败: {e}")
-    
+                self.printer(f"串口 {port} 初始化失败: {e}")
+
     def set_damping_mode(self):
         # 设置读取角度的舵机为阻尼模式
-        print("设置夹爪读取角度的舵机为阻尼模式")
+        self.printer("设置夹爪读取角度的舵机为阻尼模式")
         self.uart_managers_gripper[0].set_damping(self.SERVO_IDS_GRIPPER[0], 10)
-        print(f"舵机 {self.SERVO_IDS_GRIPPER[0]} 阻尼模式设置完成")
+        self.printer(f"舵机 {self.SERVO_IDS_GRIPPER[0]} 阻尼模式设置完成")
 
         # 设置所有舵机为阻尼模式
-        print("设置机械臂舵机为阻尼模式")
+        self.printer("设置机械臂舵机为阻尼模式")
         for i, uservo in enumerate(self.uart_managers_arm):
             uservo.set_damping(self.SERVO_IDS_ARM[i], 50)
-            print(f"舵机 {self.SERVO_IDS_ARM[i]} 阻尼模式设置完成")
+            self.printer(f"舵机 {self.SERVO_IDS_ARM[i]} 阻尼模式设置完成")
 
-    
-
-    def get_robot_data(self):
-        # Shared data between threads
-        joint_pos = [0, 0, 0, 0, 0, 0]
-        grip_percentage = [0] 
-        def read_servo_angle(index, uservo, servo_id):
-            angle = uservo.query_servo_angle(servo_id)
-            if index == 2:
-                joint_pos[index] = self.SLAVE_HOME_POS_ARM[index] + (angle - self.MASTER_HOME_POS_ARM[index])
-            else:
-                joint_pos[index] = self.SLAVE_HOME_POS_ARM[index] - (angle - self.MASTER_HOME_POS_ARM[index])
-
-        def read_gripper_angle():
-            angle_gripper = self.uart_managers_gripper[0].query_servo_angle(self.SERVO_IDS_GRIPPER[0])
-            grip_percentage[0] = (self.MASTER_OPEN - angle_gripper) / (self.MASTER_OPEN - self.MASTER_CLOSE)
-
-        threads = []
-        for i, uservo in enumerate(self.uart_managers_arm):
-            t = threading.Thread(
-                target=read_servo_angle,
-                args=(i, uservo, self.SERVO_IDS_ARM[i])
-            )
-            threads.append(t)
-            t.start()
-
-        gripper_thread = threading.Thread(target=read_gripper_angle)
-        threads.append(gripper_thread)
-        gripper_thread.start()
-
-        for t in threads:
+    def stop_read_robot_data(self):
+        for t in self.threads:
             t.join()
 
-        slave_angle = grip_percentage[0] * (self.SLAVE_CLOSE - self.SLAVE_OPEN) + self.SLAVE_OPEN
+    def get_robot_data(self):
+        slave_angle = (
+            self.grip_percentage[0] * (self.SLAVE_CLOSE - self.SLAVE_OPEN)
+            + self.SLAVE_OPEN
+        )
         self.uart_managers_gripper[1].set_servo_angle(
             self.SERVO_IDS_GRIPPER[1], slave_angle, interval=0
         )
 
-        return joint_pos, grip_percentage[0]
+        return self.joint_pos, self.grip_percentage[0]
+
+    def start_read_robot_data(self):
+        # Shared data between threads
+        self.joint_pos = [0, 0, 0, 0, 0, 0]
+        self.grip_percentage = [0]
+        self.read_data_state = 1
+
+        def read_servo_angle(index, uservo, servo_id):
+            while self.read_data_state:
+                angle = uservo.query_servo_angle(servo_id)
+                if index == 2:
+                    self.joint_pos[index] = self.SLAVE_HOME_POS_ARM[index] + (
+                        angle - self.MASTER_HOME_POS_ARM[index]
+                    )
+                else:
+                    self.joint_pos[index] = self.SLAVE_HOME_POS_ARM[index] - (
+                        angle - self.MASTER_HOME_POS_ARM[index]
+                    )
+                time.sleep(self.timer_period)
+
+        def read_gripper_angle():
+            while self.read_data_state:
+                angle_gripper = self.uart_managers_gripper[0].query_servo_angle(
+                    self.SERVO_IDS_GRIPPER[0]
+                )
+                self.grip_percentage[0] = (self.MASTER_OPEN - angle_gripper) / (
+                    self.MASTER_OPEN - self.MASTER_CLOSE
+                )
+                time.sleep(self.timer_period)
+
+        self.threads = []
+        for i, uservo in enumerate(self.uart_managers_arm):
+            t = threading.Thread(
+                target=read_servo_angle, args=(i, uservo, self.SERVO_IDS_ARM[i])
+            )
+            self.threads.append(t)
+            t.start()
+
+        gripper_thread = threading.Thread(target=read_gripper_angle)
+        self.threads.append(gripper_thread)
+        gripper_thread.start()
 
 
-class PuppetRobot():
+class PuppetRobot:
     def __init__(self, robot_ip):
         self.robot_ip = robot_ip
         self.robot = Robot.RPC(self.robot_ip)
-    
 
     def initialize_robot(self, joint_pos_o):
         # 机器人初始化
-        print("初始化从机械臂")
+        self.printer("初始化从机械臂")
         ret = self.robot.MoveJ(joint_pos_o, tool=0, user=0, vel=10)
-        print("从机械臂就位", ret)
+        self.printer("从机械臂就位", ret)
         # error, joint_pos = self.robot.GetActualJointPosDegree()
         # if error != 0:
-        #     print(f"获取机器人当前关节位置失败，错误码: {error}")
+        #     self.printer(f"获取机器人当前关节位置失败，错误码: {error}")
         #     sys.exit(1)
-        # print("机器人当前关节位置", joint_pos)
-    
+        # self.printer("机器人当前关节位置", joint_pos)
+
     def robot_control(self, joint_pos):
         ret = self.robot.ServoJ(joint_pos, cmdT=0.008)
-
-     
-
-
